0b6b97da1b2a3396128e2881a0de87eb803793e5
Fehiga3456 <56139596+Fehiga3456@users.noreply.github.com>
2020-06-25 12:36:03 -0300

    Add files via upload

---
+++ Sistema_conversa/ConversatIonScript/Conversation_NodeGraph.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+[CreateAssetMenu]
+public class Conversation_NodeGraph : NodeGraph { 
+	public StartNode GetStartingNode()
+    {
+        return (StartNode)this.nodes.Find(n => n.name == "Start");
+    }
+}
